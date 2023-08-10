110cc5f364cb67a18867bdb9f4234baa0e0967b6
Michaelwu0905 <122675533+Michaelwu0905@users.noreply.github.com>
2023-08-10 19:27:48 +0800

    Update p1125 笨小猴.cpp

--- p1125 笨小猴.cpp
+++ p1125 笨小猴.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-int num[27];
-int Max = 0, Min = 300;
+int num[27]; //存储各个字母的出现次数
+int Max = 0, Min = 300;  //存储出现次数最多和最少的字母的数量
 
-bool isPrime(int x)
+bool isPrime(int x)  //质数判断函数
 {
     if (x == 0 || x == 1)
         return false;
@@ -20,11 +20,11 @@ int main()
     cin >> word;
     for (int i = 0; i < word.length(); i++)
     {
-        num[word[i] - 'a']++;
+        num[word[i] - 'a']++;  //将出现的各个字母按次数加到数组里
     }
     for (int i = 0; i < 26; i++)
     {
-        if (num[i] >= 1 && num[i] > Max)
+        if (num[i] >= 1 && num[i] > Max) //  >=1是为了当字母至少出现过一次，才能被判断为最大或最小值
             Max = num[i];
         if (num[i] >= 1 && num[i] < Min)
             Min = num[i];
